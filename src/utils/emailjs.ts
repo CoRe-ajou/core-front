e51384a75315c54725bcf98e3e2f010b5a4c73de
Hye <hye@hpcnt.com>
2021-02-04 22:41:20 +0900

    refactor: emailjs util 분리

---
+++ src/utils/emailjs.ts
@@ -0,0 +1,7 @@
+import { init } from 'emailjs-com';
+
+const USER_ID = 'user_31qB4cbozFu8ri8yb8wY4';
+export const SERVICE_ID = 'service_2o0zu5l';
+export const TEMPLATE_ID = 'template_icsgina';
+
+init(USER_ID);
